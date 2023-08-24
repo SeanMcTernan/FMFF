e9e83e2b1a3f766e3a40b95e19d422a48c5a2f48
Sean Mc Ternan <seanmcternan@gmail.com>
2023-08-24 10:50:48 -0600

    Update Site logo to FMFF logo

---
+++ src/assets/logos/FMFFLogo.jsx
@@ -0,0 +1,21 @@
+export const FMFFLogo = () => (
+  <svg
+    width="60"
+    height="60"
+    viewBox="10 0 10 50"
+    xmlns="http://www.w3.org/2000/svg"
+    className="fill-customPrimary"
+  >
+    <path d="M20,0C9,0,0,9,0,20s9,20,20,20s20-9,20-20S31,0,20,0z M9.6,15.7H7.1v2.7h2.3v2.7H7.1V27H3.4V12.9h6.2V15.7z M21.8,27h-3.2
+	l0-9.5L17.3,27H15l-1.3-9.3l0,9.3h-3.2V12.9h4.7c0.1,0.8,0.3,1.8,0.4,3l0.5,3.6l0.8-6.6h4.8V27z M29.4,15.7h-2.5v2.7h2.3v2.7h-2.3
+	V27h-3.7V12.9h6.2V15.7z M36.2,18.4v2.7H34V27h-3.7V12.9h6.2v2.8H34v2.7H36.2z"/>
+  </svg>
+);
+
+
+
+
+
+
+
+
